4975d275744de949babdaf74973d0a82e0d99e04
Terry WS Chan <t.chan@nki.nl>
2025-06-13 10:03:33 +0200

    Fixed exercises formatting: show floating TOC, use numbered sections

--- compiling tasks.R
+++ compiling tasks.R
@@ -7,10 +7,20 @@ savingfolder <- paste0(mainfolder,'/_book/exercises/')
 for (chapter in chapter.folders){
   tasks <- list.files(chapter)
   for (task in tasks){
+    if(!grepl("\\.Rmd$", task)) {
+      message(sprintf("Skipping file '%s' (not with extension .Rmd)...", task))
+      next
+    }
     nametask <- substr(task,1,nchar(task)-4)
     for (version in 1:2){
       param = parameters[version]
-      rmarkdown::render(input = paste(chapter,task,sep='/'), output_format ="html_document" , output_file = paste0(savingfolder,nametask,names[version],'.html'))
+      rmarkdown::render(input = paste(chapter,task,sep='/'),
+                        output_format = rmarkdown::html_document(
+                          toc=TRUE,
+                          toc_float=list(collapsed=FALSE),
+                          number_sections=TRUE
+                        ),
+                        output_file = paste0(savingfolder,nametask,names[version],'.html'))
     }
   }
 }
